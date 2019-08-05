ceda7be47fe3c6b2ac470c01b3230e067e714018
juan.garza@axented.com <juan.garza@axented.com>
2019-08-05 11:22:01 -0500

    Initial Commit

---
+++ clift_iOSTests/clift_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  clift_iOSTests.swift
+//  clift_iOSTests
+//
+//  Created by Juan Carlos Garza on 8/5/19.
+//  Copyright © 2019 Clift. All rights reserved.
+//
+
+import XCTest
+@testable import clift_iOS
+
+class clift_iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
