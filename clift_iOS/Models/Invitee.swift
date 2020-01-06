f5232b9c4499a3d23bf5d7d8cb2373ac1c1ba7a5
Juan <juan.garza@axented.com>
2020-01-06 11:41:36 -0600

    Add invitee model

--- clift_iOS/Models/Invitee.swift
+++ clift_iOS/Models/Invitee.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import UIKit
+import ObjectMapper
+import ObjectMapper_Realm
+import RealmSwift
+
+class Invitee: Mappable {
+    var name = ""
+    var email = ""
+    var cellphoneNumber = ""
+    var isInvited: Int? = nil
+    var isConfirmed: Bool? = nil
+    var event = Event()
+    var user = User()
+    
+    var errors: [String] = []
+    
+    convenience required init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        name <- map["name"]
+        email <- map["email"]
+        cellphoneNumber <- map["cellphone"]
+        isInvited <- map["is_invited"]
+        isConfirmed <- map["is_confirmed"]
+        
+        
+        if let unwrappedErrors = map.JSON["errors"] as? [String] {
+            for error in unwrappedErrors {
+                errors.append(error)
+            }
+        }
+    }
+}
