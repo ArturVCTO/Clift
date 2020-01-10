69ac7f3807c34ff77652f2ad7f0f925646045176
Juan <juan.garza@axented.com>
2020-01-10 16:39:54 -0600

    Add Addresses VC files

---
+++ clift_iOS/ViewControllers/UserMenu/UserSettings/Addresses/AddAddressViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddAddressViewController.swift
+//  clift_iOS
+//
+//  Created by Juan Carlos Garza on 1/10/20.
+//  Copyright © 2020 Clift. All rights reserved.
+//
+
+import Foundation
---
+++ clift_iOS/ViewControllers/UserMenu/UserSettings/Addresses/AddressesTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddressesTableViewController.swift
+//  clift_iOS
+//
+//  Created by Juan Carlos Garza on 1/10/20.
+//  Copyright © 2020 Clift. All rights reserved.
+//
+
+import Foundation
