103f8c439d27b9a92fcd2ee6a66a2e3822c407b0
Juan <juan.garza@axented.com>
2020-01-10 16:48:15 -0600

    Add outlet variables to AddAddressVC

--- clift_iOS/ViewControllers/UserMenu/UserSettings/Addresses/AddAddressViewController.swift
+++ clift_iOS/ViewControllers/UserMenu/UserSettings/Addresses/AddAddressViewController.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import UIKit
+import TextFieldEffects
+
+class AddAddressViewController: UIViewController {
+    @IBOutlet weak var firstNameTextField: HoshiTextField!
+    @IBOutlet weak var lastNameTextField: HoshiTextField!
+    @IBOutlet weak var cellphoneTextField: HoshiTextField!
+    @IBOutlet weak var emailTextField: HoshiTextField!
+    @IBOutlet weak var addressTextField: HoshiTextField!
+    @IBOutlet weak var districtTextField: HoshiTextField!
+    @IBOutlet weak var countryButton: UIButton!
+    @IBOutlet weak var stateButton: UIButton!
+    @IBOutlet weak var cityButton: UIButton!
+    @IBOutlet weak var zipcodeTextField: UITextField!
+    var address = Address()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
