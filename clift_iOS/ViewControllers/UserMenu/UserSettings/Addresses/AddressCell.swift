6dbed3decce6de33187805644d07988d35eabb9b
Juan <juan.garza@axented.com>
2020-01-13 10:32:12 -0600

    Add Address cell and addresses table vc files to project

---
+++ clift_iOS/ViewControllers/UserMenu/UserSettings/Addresses/AddressCell.swift
@@ -0,0 +1,9 @@
+//
+//  AddressCell.swift
+//  clift_iOS
+//
+//  Created by Juan Carlos Garza on 1/10/20.
+//  Copyright © 2020 Clift. All rights reserved.
+//
+
+import Foundation
--- clift_iOS/ViewControllers/UserMenu/UserSettings/Addresses/AddressesTableViewController.swift
+++ clift_iOS/ViewControllers/UserMenu/UserSettings/Addresses/AddressesTableViewController.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+import UIKit
+
+class AddressesTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
