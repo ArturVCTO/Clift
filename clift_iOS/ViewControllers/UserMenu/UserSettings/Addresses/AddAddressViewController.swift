0bf9703ef341a173fb66dbedd007d782e8eb355d
Juan <juan.garza@axented.com>
2020-01-14 14:57:56 -0600

    Add address function added to button

--- clift_iOS/ViewControllers/UserMenu/UserSettings/Addresses/AddAddressViewController.swift
+++ clift_iOS/ViewControllers/UserMenu/UserSettings/Addresses/AddAddressViewController.swift
@@ -21,9 +21,50 @@ class AddAddressViewController: UIViewController {
     @IBOutlet weak var stateButton: UIButton!
     @IBOutlet weak var cityButton: UIButton!
     @IBOutlet weak var zipcodeTextField: UITextField!
-    var address = Address()
+    var address: Address? = Address()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.firstNameTextField.delegate = self
+        self.lastNameTextField.delegate = self
+        self.emailTextField.delegate = self
+        self.addressTextField.delegate = self
+        self.districtTextField.delegate = self
+        self.zipcodeTextField.delegate = self
+    }
+    
+    func addAddress(address: Address) {
+        sharedApiManager.addAddress(address: address) { (address, result) in
+            if let response = result {
+                if (response.isSuccess()) {
+                    self.navigationController?.popViewController(animated: true)
+                }
+            }
+        }
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let address = self.address else { return }
+        self.addAddress(address: address)
+    }
+}
+extension AddAddressViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch textField {
+        case textField == firstNameTextField:
+            self.address?.firstName = self.firstNameTextField.text!
+        case textField == lastNameTextField:
+            self.address?.lastName = self.lastNameTextField.text!
+        case textField == cellphoneTextField:
+            self.address?.cellPhoneNumber = self.cellphoneTextField.text!
+        case textField == emailTextField:
+            self.address?.email = self.emailTextField.text!
+        case textField == addressTextField:
+            self.address?.streetAndNumber = self.addressTextField.text!
+        case textField == districtTextField:
+            self.address?.suburb = self.addressTextField.text!
+        case textField == zipcodeTextField:
+            self.address?.zipCode = self.zipcodeTextField.text!
+        }
     }
 }
