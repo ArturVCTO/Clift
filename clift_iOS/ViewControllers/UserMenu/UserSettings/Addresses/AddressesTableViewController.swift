f57813092930c2f35c6b46a515ccaeacd374c624
Juan <juan.garza@axented.com>
2020-01-16 09:41:03 -0600

    Add swipe make default and delete actions to addresses list

--- clift_iOS/ViewControllers/UserMenu/UserSettings/Addresses/AddressesTableViewController.swift
+++ clift_iOS/ViewControllers/UserMenu/UserSettings/Addresses/AddressesTableViewController.swift
@@ -28,12 +28,27 @@ class AddressesTableViewController: UITableViewController {
         }
     }
     
+    func updateDefaultAddress(address: Address) {
+        sharedApiManager.setDefaultAddress(address: address) { (updatedAddress, result) in
+            if let response = result {
+                if (response.isSuccess()) {
+                    self.getAddresses()
+                    self.tableView.reloadData()
+                } else {
+                    self.showMessage("\(updatedAddress!.errors.first ?? "Error al actualizar")", type: .error)
+                }
+            }
+        }
+    }
+    
     @IBAction func addAddressesButtonTapped(_ sender: Any) {
         if #available(iOS 13.0, *) {
            let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(identifier: "addAddressVC") as! AddAddressViewController
+            vc.addressesTableVC = self
            self.navigationController?.pushViewController(vc, animated: true)
         } else {
            let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "addAddressVC") as! AddAddressViewController
+            vc.addressesTableVC = self
            self.navigationController?.pushViewController(vc, animated: true)
        }
     }
@@ -46,6 +61,23 @@ class AddressesTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "addressTVCell", for: indexPath) as! AddressCell
         cell.setup(address: addresses[indexPath.row])
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let makeDefaultTitle = NSLocalizedString("Marcar como predeterminado", comment: "Make Default action")
+        let deleteTitle = NSLocalizedString("Borrar", comment: "Delete action")
+
+        let makeDefaultAction = UITableViewRowAction(style: .normal,
+          title: makeDefaultTitle) { (action, indexPath) in
+            self.updateDefaultAddress(address: self.addresses[indexPath.row])
+        }
+        
+        let deleteAction = UITableViewRowAction(style: .destructive,
+          title: deleteTitle) { (action, indexPath) in
+            self.updateDefaultAddress(address: self.addresses[indexPath.row])
+        }
+        
+        return [makeDefaultAction, deleteAction]
     }
 }
 
