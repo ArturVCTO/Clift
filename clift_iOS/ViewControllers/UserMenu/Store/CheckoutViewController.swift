f03ceee6a3f3ae14dd8950415ad806216a51fbd3
Juan <juan.garza@axented.com>
2020-01-16 13:25:59 -0600

    Add CheckoutVC file and relate it to Storyboard View

---
+++ clift_iOS/ViewControllers/UserMenu/Store/CheckoutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CheckoutViewController.swift
+//  clift_iOS
+//
+//  Created by Juan Carlos Garza on 1/16/20.
+//  Copyright © 2020 Clift. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CheckoutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
