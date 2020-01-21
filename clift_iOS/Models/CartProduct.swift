aa2180187f168ea380ab441428d108153ce5e7ff
Juan <juan.garza@axented.com>
2020-01-21 15:51:58 -0600

    Change swift version in pods

---
+++ clift_iOS/Models/CartProduct.swift
@@ -0,0 +1,9 @@
+//
+//  CartProduct.swift
+//  clift_iOS
+//
+//  Created by Juan Carlos Garza on 1/16/20.
+//  Copyright © 2020 Clift. All rights reserved.
+//
+
+import Foundation
---
+++ clift_iOS/Network/StripeApiClient.swift
@@ -0,0 +1,9 @@
+//
+//  StripeApiClient.swift
+//  clift_iOS
+//
+//  Created by Juan Carlos Garza on 1/17/20.
+//  Copyright © 2020 Clift. All rights reserved.
+//
+
+import Foundation
