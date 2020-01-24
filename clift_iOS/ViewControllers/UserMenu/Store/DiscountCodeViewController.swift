cfe5fdfcc131b2945ef9c2b8ad6ce1a9453b825d
Juan <juan.garza@axented.com>
2020-01-24 10:40:43 -0600

    Add discount code view controller

---
+++ clift_iOS/ViewControllers/UserMenu/Store/DiscountCodeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DiscountCodeViewController.swift
+//  clift_iOS
+//
+//  Created by Juan Carlos Garza on 1/23/20.
+//  Copyright © 2020 Clift. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DiscountCodeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ clift_iOS/ViewControllers/UserMenu/Store/PaymentTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PaymentTableViewController.swift
+//  clift_iOS
+//
+//  Created by Juan Carlos Garza on 1/22/20.
+//  Copyright © 2020 Clift. All rights reserved.
+//
+
+import Foundation
