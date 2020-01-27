380dbcf7bad8c91af56de18aaa9b1765d8a06ad8
Juan <juan.garza@axented.com>
2020-01-27 05:48:20 -0600

    Add gift cell handling

--- clift_iOS/ViewControllers/UserMenu/Registry/Cells/GiftSummaryCell.swift
+++ clift_iOS/ViewControllers/UserMenu/Registry/Cells/GiftSummaryCell.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+import UIKit
+
+class GiftSummaryCell: UITableViewCell {
+    @IBOutlet weak var giftImageView: UIImageView!
+    @IBOutlet weak var gifterEmailLabel: UILabel!
+    @IBOutlet weak var gifterDateLabel: UILabel!
+    @IBOutlet weak var giftCategoryLabel: UILabel!
+    
+    func configure(with gift: MockGift) {
+        self.giftImageView.image = gift.image
+        if gift.giftersEmail.count > 1 {
+            self.gifterEmailLabel.text = "\(gift.giftersEmail.first ?? ""), +\(gift.giftersEmail.count - 1)"
+        } else {
+            self.gifterEmailLabel.text = gift.giftersEmail.first
+        }
+        self.gifterDateLabel.text = gift.dateGifted
+        self.giftCategoryLabel.text = gift.category
+    }
+}
