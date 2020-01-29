d85e8b75a1efb3caee7a19fd18c7a49008d8fa7e
Juan <juan.garza@axented.com>
2020-01-29 10:48:19 -0600

    Add thank message delegates and send thank message function

--- clift_iOS/ViewControllers/UserMenu/Registry/ThankGuestViewController.swift
+++ clift_iOS/ViewControllers/UserMenu/Registry/ThankGuestViewController.swift
@@ -19,18 +19,60 @@ class ThankGuestViewController: UIViewController {
     @IBOutlet weak var giftButton: UIButton!
     @IBOutlet weak var giftMessageTextField: HoshiTextField!
     var thankMessage: ThankMessage? = nil
+    var gift: MockGift?
+    var emailersDropDown = DropDown()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.giftMessageTextField.delegate = self
+        self.getGiftInformation(gift: self.gift!)
     }
     
-    func getGiftInformation() {
+    func loadDropDownInfo(string: [String]) {
+        var dataSourceDropdown = [String]()
+        emailersDropDown.anchorView = self.giftButton
         
+        for string in string {
+            dataSourceDropdown.append(string)
+        }
+        emailersDropDown.dataSource = dataSourceDropdown
+        emailersDropDown.bottomOffset = CGPoint(x: 0, y: giftButton.bounds.height)
+        
+        emailersDropDown.selectionAction = { [weak self] (index, item) in
+            self!.giftButton.setTitle(item, for: .normal)
+            self?.thankMessage?.email = string[index]
+        }
+    }
+    
+    func getGiftInformation(gift: MockGift) {
+        self.giftImage.image = gift.image
+        self.giftName.text = gift.giftName
+        self.giftShop.text = gift.shop
+        self.giftPrice.text = "\(gift.giftPrice)"
+        self.loadDropDownInfo(string: gift.giftersEmail)
+    }
+    
+    func sendThankMessage(thankMessage: ThankMessage, event: Event,eventProduct: EventProduct) {
+        sharedApiManager.sendThankMessage(thankMessage: thankMessage, event: event, eventProduct: eventProduct) { (emptyObject, result) in
+            if let response = result {
+                if response.isSuccess() {
+                    self.navigationController?.popViewController(animated: true)
+                    self.navigationController!.showMessage("Mensaje enviado con éxito", type: .success)
+                } else if response.isClientError() {
+                    self.showMessage("Hubo un error enviando el correo.", type: .error)
+                } else {
+                    self.showMessage("Hubo un error enviando el correo.", type: .error)
+                }
+            }
+        }
     }
     
     @IBAction func sendMessageTapped(_ sender: Any) {
-        self.showMessage("Mensaje enviado con éxito", type: .success)
-        self.navigationController?.popViewController(animated: true)
+        
+    }
+    
+    @IBAction func giftersEmailButtonTapped(_ sender: Any) {
+        self.emailersDropDown.show()
     }
     
     @IBAction func cancelButtonTapped(_ sender: Any) {
@@ -38,5 +80,12 @@ class ThankGuestViewController: UIViewController {
     }
 }
 extension ThankGuestViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.thankMessage?.thankMessage = giftMessageTextField.text
+    }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {   //delegate method
+       textField.resignFirstResponder()
+       return true
+    }
 }
