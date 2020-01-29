7309592ea333fa2cdcb927755670720855fa67b8
Juan <juan.garza@axented.com>
2020-01-29 11:00:50 -0600

    Add Update quantity in checkout

--- clift_iOS/ViewControllers/UserMenu/Store/CheckoutProductCell.swift
+++ clift_iOS/ViewControllers/UserMenu/Store/CheckoutProductCell.swift
@@ -14,11 +14,15 @@ class CheckoutProductCell: UITableViewCell {
     @IBOutlet weak var productImageView: UIImageView!
     @IBOutlet weak var productCostLabel: UILabel!
     @IBOutlet weak var productQuantityLabel: UILabel!
+    var productQuantity: Int?
+    var productPrice: Double?
     
      func configure(with product: MockProduct) {
         self.productNameLabel.text = product.name
         self.productImageView.image = product.image
         self.productCostLabel.text = "\(self.getPriceStringFormat(value: product.price))"
+        self.productQuantity = product.quantity
+        self.productPrice = product.price
         self.productQuantityLabel.text = "\(product.quantity)"
     }
     
@@ -27,5 +31,24 @@ class CheckoutProductCell: UITableViewCell {
       formatter.numberStyle = .currency
       
       return formatter.string(from: NSNumber(value: value))!
+    }
+    
+    
+    @IBAction func addProductQuantity(_ sender: Any) {
+        let newQuant = self.productQuantity! + 1
+        self.productQuantity = newQuant
+        self.productCostLabel.text = "\(self.getPriceStringFormat(value: productPrice! * Double(newQuant)))"
+        self.productQuantityLabel.text = "\(newQuant)"
+    }
+    
+    @IBAction func reduceProductQuantity(_ sender: Any) {
+        if self.productQuantity! < 1 {
+            self.showMessage("No se puede reducir a 0.", type: .error)
+        } else {
+            let newQuant = (self.productQuantity)! - 1
+            self.productQuantity = newQuant
+            self.productCostLabel.text = "\(self.getPriceStringFormat(value: productPrice! * Double(newQuant)))"
+            self.productQuantityLabel.text = "\(newQuant)"
+        }
     }
 }
