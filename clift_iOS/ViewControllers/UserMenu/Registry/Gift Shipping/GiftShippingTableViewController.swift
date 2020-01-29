a14fb5ae97043060a7f6f62107b770ac031c50ee
Juan <juan.garza@axented.com>
2020-01-29 10:43:04 -0600

    Add gift shipping table view controller

--- clift_iOS/ViewControllers/UserMenu/Registry/Gift Shipping/GiftShippingTableViewController.swift
+++ clift_iOS/ViewControllers/UserMenu/Registry/Gift Shipping/GiftShippingTableViewController.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+
+class GiftShippingTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+    }
+    
+    @IBAction func acceptButtonTapped(_ sender: Any) {
+        if #available(iOS 13.0, *) {
+              let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(identifier: "giftShippingConfirmationVC") as! GiftShippingConfirmationViewController
+              self.navigationController?.pushViewController(vc, animated: true)
+          } else {
+            // Fallback on earlier versions
+            let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "giftShippingConfirmationVC") as! GiftShippingConfirmationViewController
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
