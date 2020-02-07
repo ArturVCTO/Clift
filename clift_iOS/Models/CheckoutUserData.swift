9a1bea53a81f3ed97223ef17b320cb96f5664a66
Juan <juan.garza@axented.com>
2020-02-06 19:35:56 -0600

    Add checkout user data model

--- clift_iOS/Models/CheckoutUserData.swift
+++ clift_iOS/Models/CheckoutUserData.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+import ObjectMapper
+import ObjectMapper_Realm
+import RealmSwift
+
+class CheckoutUserData: Mappable {
+    var id: String? = ""
+    var email: String? = ""
+    var shippingAddress: String? = ""
+    var cellPhoneNumber: String? = ""
+    
+    convenience required init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        id <- map["id"]
+        email <- map["email"]
+        shippingAddress <- map["shipping_address"]
+        cellPhoneNumber <- map["cell_phone_number"]
+    }
+}
