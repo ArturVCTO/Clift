57cdff971ec41b3053b7c5cc26a71c1f6c350a9a
Juan <juan.garza@axented.com>
2020-02-06 19:31:41 -0600

    Add checkout model to project

--- clift_iOS/Models/Checkout.swift
+++ clift_iOS/Models/Checkout.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+import ObjectMapper
+import ObjectMapper_Realm
+import RealmSwift
+
+class Checkout: Mappable {
+    var id: String? = ""
+    var cartItemIds: [String] = []
+    var userData: CheckoutUserData?
+    var message: String? = ""
+    
+    convenience required init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        id <- map["id"]
+        cartItemIds <- map["shopping_cart_items"]
+        userData <- map["user_data"]
+        message <- map["message"]
+    }
+}
