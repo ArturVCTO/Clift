3947cf6baffb73fb5862d1a39d83b27680eff62d
LizzieGuajardoMozo <lizziemgm97@gmail.com>
2020-08-04 14:26:26 -0500

    Fix Add Guest Manually
    Add guest manually now includes plus one field and address
    email and phone fields need to be valid
    All fields must be filled

--- clift_iOS/ViewControllers/UserMenu/Registry/Invitations/AddGuestsViewController.swift
+++ clift_iOS/ViewControllers/UserMenu/Registry/Invitations/AddGuestsViewController.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 import UIKit
-import GoogleSignIn
 import TextFieldEffects
 import GSMessages
 
@@ -20,65 +19,94 @@ class AddGuestsViewController: UIViewController{
     
     var delegate:updateNewGuestDelegate!
     
-    @IBOutlet weak var signInButton: GIDSignInButton!
-    @IBOutlet weak var statusText: UILabel!
-    @IBOutlet weak var signOutButton: UIButton!
-    @IBOutlet weak var disconnectButton: UIButton!
     @IBOutlet weak var guestNameTextField: HoshiTextField!
     @IBOutlet weak var guestEmailTextField: HoshiTextField!
     @IBOutlet weak var guestTelephoneTextField: HoshiTextField!
+    @IBOutlet weak var guestHasPlusOneSwitch: UISwitch!
+    @IBOutlet weak var guestPlusOneNameTextField: HoshiTextField!
+    @IBOutlet weak var guestAdressTextField: HoshiTextField!
+    private var currentTextField: UITextField!
     var vc: GuestListViewController!
     var newGuest = EventGuest()
     var currentEvent = Event()
+    var yViewOrigin : CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.guestNameTextField.delegate = self
         self.guestEmailTextField.delegate = self
         self.guestTelephoneTextField.delegate = self
-        GIDSignIn.sharedInstance()?.presentingViewController = self
-        
-        NotificationCenter.default.addObserver(self,
-               selector: #selector(receiveToggleAuthUINotification(_:)),
-               name: NSNotification.Name(rawValue: "ToggleAuthUINotification"),
-               object: nil)
+        self.guestPlusOneNameTextField.delegate = self
+        self.guestAdressTextField.delegate = self
+       NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+       NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
 
-           statusText.text = "Initialized Swift app..."
-           toggleAuthUI()
         let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboardOnClickOutside))
         view.addGestureRecognizer(tap)
+        self.guestPlusOneNameTextField.isHidden = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(false)
+        yViewOrigin = self.view.frame.origin.y
     }
     
     @IBAction func backButtonTapped(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
-    
-    func toggleAuthUI() {
-        if let _ = GIDSignIn.sharedInstance()?.currentUser?.authentication {
-          // Signed in
-            signInButton.isHidden = true
-            signOutButton.isHidden = false
-            disconnectButton.isHidden = false
-        } else {
-          signInButton.isHidden = false
-            signOutButton.isHidden = true
-            disconnectButton.isHidden = true
-          statusText.text = "Google Sign in\niOS Demo"
+       
+    @IBAction func addGuestButtonTapped(_ sender: Any) {
+        if let currentTextField = currentTextField{
+            currentTextField.resignFirstResponder()
         }
+        if  newGuest.name == "" || newGuest.email == "" || newGuest.cellPhoneNumber == "" || (newGuest.has_plus_one && newGuest.companion_name == "") || newGuest.address == ""{
+            
+            self.showMessage(NSLocalizedString("Favor de llenar todos los campos", comment: "Guest Registry Error"),type: .error)
+            return
+        }
+        if(!guestEmailTextField.text!.isValidEmail()){
+            self.showMessage(NSLocalizedString("Correo electrónico inválido", comment: "Invalid Guest Email"),type: .error)
+            return
+        }
+        if(!guestTelephoneTextField.text!.isValidPhone()){
+            self.showMessage(NSLocalizedString("Número telefónico inválido", comment: "Invalid Guest Phone"),type: .error)
+            return
+        }
+        self.addGuest(event: self.currentEvent, guest: self.newGuest)
     }
     
-    @objc func receiveToggleAuthUINotification(_ notification: NSNotification) {
-        if notification.name.rawValue == "ToggleAuthUINotification" {
-            self.toggleAuthUI()
-            if notification.userInfo != nil {
-                guard let userInfo = notification.userInfo as? [String: String] else { return }
-                self.statusText.text = userInfo["statusText"]!
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if (self.guestNameTextField.isEditing || self.guestEmailTextField.isEditing || self.guestTelephoneTextField.isEditing) {
+            return
+        } else {
+            if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+                if self.view.frame.origin.y == self.yViewOrigin {
+                    self.view.frame.origin.y -= (keyboardSize.height)
+                }
             }
         }
     }
-    
-    @IBAction func addGuestButtonTapped(_ sender: Any) {
-        self.addGuest(event: self.currentEvent, guest: self.newGuest)
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != self.yViewOrigin {
+            self.view.frame.origin.y = self.yViewOrigin
+        }
     }
+    
+    @IBAction func plusOneSwitchOnChange(_ sender: UISwitch) {
+        if sender.isOn
+        {
+            self.guestPlusOneNameTextField.isHidden = false
+            self.newGuest.has_plus_one = true
+        }
+        else{
+            self.guestPlusOneNameTextField.isHidden = true
+            self.newGuest.has_plus_one = false
+            self.newGuest.companion_name = ""
+        }
+    }
+    
+    
     
     func addGuest(event: Event, guest: EventGuest) {
         sharedApiManager.addGuest(event: event, guest: guest) { (emptyObject, result) in
@@ -98,29 +126,17 @@ class AddGuestsViewController: UIViewController{
     }
     
     
-    
-    
-    @IBAction func didTapDisconnect(_ sender: Any) {
-        GIDSignIn.sharedInstance()?.disconnect()
-        
-        statusText.text = "Disconnecting."
-    }
-    
-    @IBAction func didTapSignOut(_ sender: Any) {
-        GIDSignIn.sharedInstance()?.signOut()
-        
-        statusText.text = "Signed out."
-        toggleAuthUI()
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self,
-        name: NSNotification.Name(rawValue: "ToggleAuthUINotification"),
-        object: nil)
-    }
 }
 extension AddGuestsViewController: UITextFieldDelegate {
    
+    func textfieldShouldReturn(textField: UITextField)->Bool{
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        currentTextField = textField
+    }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
       textField.resignFirstResponder()
@@ -130,15 +146,19 @@ extension AddGuestsViewController: UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         if textField == guestNameTextField {
           self.newGuest.name = self.guestNameTextField.text!
+            self.newGuest.isConfirmed = "invitation_not_sent"
         }
-        
         if textField == guestEmailTextField {
             self.newGuest.email = self.guestEmailTextField.text!
         }
-        
-      if textField == guestTelephoneTextField {
-            self.newGuest.cellPhoneNumber = guestTelephoneTextField.text!
+        if textField == guestTelephoneTextField {
+            self.newGuest.cellPhoneNumber = self.guestTelephoneTextField.text!
         }
-      
+        if textField == guestPlusOneNameTextField {
+            self.newGuest.companion_name = self.guestPlusOneNameTextField.text!
+        }
+        if textField == guestAdressTextField {
+            self.newGuest.address = self.guestAdressTextField.text!
+        }
     }
 }
