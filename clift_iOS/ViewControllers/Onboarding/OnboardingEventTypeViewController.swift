5fc619284ff22a8f4a586057557a49208fef71ab
Juan Pablo R <jprscs@gmail.com>
2020-11-03 20:24:05 -0600

    Code formatting

--- clift_iOS/ViewControllers/Onboarding/OnboardingEventTypeViewController.swift
+++ clift_iOS/ViewControllers/Onboarding/OnboardingEventTypeViewController.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2019 Clift. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 class OnboardingEventTypeViewController: UIViewController {
--- clift_iOS/ViewControllers/Session/LoginViewController.swift
+++ clift_iOS/ViewControllers/Session/LoginViewController.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2019 Clift. All rights reserved.
 //
 
-import Foundation
 import UIKit
 import TextFieldEffects
 import RealmSwift
