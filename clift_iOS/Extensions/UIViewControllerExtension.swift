e64211b5f26e8f13b35e78b16800342ee4db85ff
Fernando Limón <fernando@kinedu.com>
2021-01-15 19:00:35 -0600

    Assets Added

---
+++ clift_iOS/Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewControllerExtension.swift
+//  clift_iOS
+//
+//  Created by Fernando Limón Flores on 12/01/21.
+//  Copyright © 2021 Clift. All rights reserved.
+//
+
+import Foundation
---
+++ clift_iOS/ViewControllers/Guest/FilterSelectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FilterSelectionViewController.swift
+//  clift_iOS
+//
+//  Created by Fernando Limón Flores on 13/01/21.
+//  Copyright © 2021 Clift. All rights reserved.
+//
+
+import UIKit
+
+class FilterSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
