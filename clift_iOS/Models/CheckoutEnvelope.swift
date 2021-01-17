6dbbbe30a09069aff832a8ee23ed05242e1f9d19
Fernando Limón <fernando@kinedu.com>
2021-01-16 22:30:31 -0600

    Assets Added

---
+++ clift_iOS/Models/CheckoutEnvelope.swift
@@ -0,0 +1,9 @@
+//
+//  CheckoutEnvelope.swift
+//  clift_iOS
+//
+//  Created by Fernando Limón Flores on 16/01/21.
+//  Copyright © 2021 Clift. All rights reserved.
+//
+
+import Foundation
---
+++ clift_iOS/ViewControllers/Guest/EnvelopeInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EnvelopeInfoViewController.swift
+//  clift_iOS
+//
+//  Created by Fernando Limón Flores on 16/01/21.
+//  Copyright © 2021 Clift. All rights reserved.
+//
+
+import UIKit
+
+class EnvelopeInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
