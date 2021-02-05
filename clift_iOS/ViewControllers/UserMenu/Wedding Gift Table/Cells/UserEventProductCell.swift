60673ec588de0e46417c83b7d679c87adad953b0
Fernando Limón <fernando@kinedu.com>
2021-02-05 17:39:41 -0600

    Files Added

---
+++ clift_iOS/ViewControllers/UserMenu/Wedding Gift Table/Cells/UserEventProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  UserEventProductCell.swift
+//  clift_iOS
+//
+//  Created by Fernando Limón Flores on 05/02/21.
+//  Copyright © 2021 Clift. All rights reserved.
+//
+
+import UIKit
+
+class UserEventProductCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
