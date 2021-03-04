698998d56763437c36f3b57bf5cd9b797b4fad44
Fernando Limón <fernando@kinedu.com>
2021-03-03 20:29:52 -0600

    StoreProductCell Added

---
+++ clift_iOS/ViewControllers/Store/Cells/StoreProductCell.swift
@@ -0,0 +1,58 @@
+//
+//  StoreProductCell.swift
+//  clift_iOS
+//
+//  Created by Fernando Limón Flores on 02/03/21.
+//  Copyright © 2021 Clift. All rights reserved.
+//
+
+import UIKit
+
+class StoreProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    @IBOutlet weak var quantityView: UIView!
+    @IBOutlet weak var productQuantityLabel: UILabel!
+    @IBOutlet weak var addCartView: UIView!
+    @IBOutlet weak var productActionButton: UIButton!
+    var cellWidthConstraint: NSLayoutConstraint?
+    
+    var cellWidth: CGFloat? {
+        didSet{
+            guard let cellWidth = cellWidth else { return }
+            cellWidthConstraint?.constant = cellWidth
+            cellWidthConstraint?.isActive = true
+            contentView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+            contentView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+            setNeedsLayout()
+            layoutIfNeeded()
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        cellWidthConstraint = contentView.widthAnchor.constraint(equalToConstant: 0)
+        setup()
+    }
+    
+    func setup() {
+        quantityView.layer.cornerRadius = 5
+        quantityView.layer.borderWidth = 1
+        quantityView.layer.borderColor = UIColor(named: "PrimaryBlue")?.cgColor
+        addCartView.layer.cornerRadius = 5
+        productImage.contentMode = .scaleAspectFit
+    }
+    
+    func configure(product: Product) {
+        
+        if let imageURL = URL(string:"\(product.imageUrl)") {
+            self.productImage.sd_setImage(with: imageURL, placeholderImage: UIImage(named: "cliftplaceholder"))
+        }
+        productNameLabel.text = product.name
+        productPriceLabel.text = "$ \(product.price) MXN"
+        //productQuantityLabel.text = "\(product.gifted_quantity) / \(product.quantity)"
+    }
+}
