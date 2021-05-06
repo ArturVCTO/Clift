11536a1fef7f34c3c4e042220d85778a5f749eb6
Fernando Limón <fernando@kinedu.com>
2021-05-06 18:10:18 -0500

    Files Added

---
+++ clift_iOS/ViewControllers/Profile/ViewControllers/ProfileBankInformationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileBankInformationViewController.swift
+//  clift_iOS
+//
+//  Created by Fernando Limón Flores on 06/05/21.
+//  Copyright © 2021 Clift. All rights reserved.
+//
+
+import UIKit
+
+class ProfileBankInformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
