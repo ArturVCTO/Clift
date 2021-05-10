09512712ce72dfa7313c25e53a5fb7c90c9ff971
Lydia <lydiamariong@gmail.com>
2021-05-10 10:32:12 -0600

    View Controller setup

--- clift_iOS/ViewControllers/Profile/ViewControllers/ProfileHomeViewController.swift
+++ clift_iOS/ViewControllers/Profile/ViewControllers/ProfileHomeViewController.swift
@@ -9,22 +9,46 @@
 import UIKit
 
 class ProfileHomeViewController: UIViewController {
-
+    
+    @IBOutlet weak var bannerImageViewButton: UIButton!
+    @IBOutlet weak var profilePictureImageView: customImageView!
+    @IBOutlet weak var profileNameTextField: UITextField!
+    @IBOutlet weak var profileAddressTextField: UITextField!
+    @IBOutlet weak var profileBankAccountTextField: UITextField!
+    var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setNavBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setNavBar() {
+        let titleLabel = UILabel()
+        titleLabel.text = "EDITAR MESA"
+        titleLabel.textColor = .white
+        titleLabel.font = UIFont(name: "Mihan-Regular", size: 16.0)!
+        titleLabel.addCharactersSpacing(5)
+        titleLabel.sizeToFit()
+        navigationItem.titleView = titleLabel
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
-    */
-
+    
+    @IBAction func profilePictureButtonPressed(_ sender: Any) {
+        //
+    }
+    
+    
+    @IBAction func bannerButtonPressed(_ sender: Any) {
+        //
+    }
+    
+    @IBAction func dateButtonPressed(_ sender: Any) {
+        // Calendar
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        // Save
+    }
+    
 }
