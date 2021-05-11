49bb36ba22da2024882916f0d382f4e146f11e43
Lydia <lydiamariong@gmail.com>
2021-05-11 14:12:02 -0600

    added image picker to profile

--- clift_iOS/ViewControllers/Profile/ViewControllers/ProfileHomeViewController.swift
+++ clift_iOS/ViewControllers/Profile/ViewControllers/ProfileHomeViewController.swift
@@ -8,18 +8,22 @@
 
 import UIKit
 
-class ProfileHomeViewController: UIViewController {
+class ProfileHomeViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var bannerImageViewButton: UIButton!
+    @IBOutlet weak var bannerPictureImageView: customImageView!
     @IBOutlet weak var profilePictureImageView: customImageView!
     @IBOutlet weak var profileNameTextField: UITextField!
     @IBOutlet weak var profileAddressTextField: UITextField!
     @IBOutlet weak var profileBankAccountTextField: UITextField!
-    var imagePicker: UIImagePickerController!
+    var profileImagePicker: UIImagePickerController?
+    var bannerImagePicker: UIImagePickerController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        profileNameTextField.delegate = self
+        profileAddressTextField.delegate = self
+        profileBankAccountTextField.delegate = self
         setNavBar()
     }
     
@@ -35,12 +39,12 @@ class ProfileHomeViewController: UIViewController {
     }
     
     @IBAction func profilePictureButtonPressed(_ sender: Any) {
-        //
+        showImagePickerControllerActionSheet(.profilePicture)
     }
     
     
     @IBAction func bannerButtonPressed(_ sender: Any) {
-        //
+        showImagePickerControllerActionSheet(.banner)
     }
     
     @IBAction func dateButtonPressed(_ sender: Any) {
@@ -51,4 +55,62 @@ class ProfileHomeViewController: UIViewController {
         // Save
     }
     
+}
+
+extension ProfileHomeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    enum PickerName {
+        case banner
+        case profilePicture
+    }
+    
+    func showImagePickerControllerActionSheet(_ picker: PickerName) {
+        let alert = UIAlertController(title: "Elige tu imagen", message: nil, preferredStyle: .actionSheet)
+        var chooseFromCamera = UIAlertAction()
+        var chooseFromGallery = UIAlertAction()
+        
+        switch picker {
+        case .profilePicture:
+            chooseFromCamera = UIAlertAction(title: "Cámara", style: .default, handler: { _ in self.showProfileImagePickerController(.camera)})
+            chooseFromGallery = UIAlertAction(title: "Galería de fotos", style: .default, handler: { _ in self.showProfileImagePickerController(.photoLibrary) })
+        case .banner:
+            chooseFromCamera = UIAlertAction(title: "Cámara", style: .default, handler: { _ in self.showBannerImagePickerController(.camera)})
+            chooseFromGallery = UIAlertAction(title: "Galería de fotos", style: .default, handler: { _ in self.showBannerImagePickerController(.photoLibrary) })
+        }
+        
+        alert.addAction(chooseFromCamera)
+        alert.addAction(chooseFromGallery)
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showProfileImagePickerController(_ sourceType: UIImagePickerController.SourceType) {
+        profileImagePicker = UIImagePickerController()
+        profileImagePicker!.delegate = self
+        profileImagePicker!.allowsEditing = true
+        profileImagePicker!.sourceType = sourceType
+        present(profileImagePicker!, animated: true, completion: nil)
+    }
+    
+    func showBannerImagePickerController(_ sourceType: UIImagePickerController.SourceType) {
+        bannerImagePicker = UIImagePickerController()
+        bannerImagePicker!.delegate = self
+        bannerImagePicker!.allowsEditing = true
+        bannerImagePicker!.sourceType = sourceType
+        present(bannerImagePicker!, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            if picker == profileImagePicker {
+                profilePictureImageView.image = editedImage
+                profileImagePicker = nil
+                
+            } else {
+                bannerPictureImageView.image = editedImage
+                bannerImagePicker = nil
+            }
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 }
